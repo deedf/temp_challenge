32365a69f28579cfd4014c043ea5d6dd64ebf29d
Didier Frick <didier@dfr.ch>
2023-12-17 11:42:26 +0100

    Add delay to push.sh to leave time for changes to apply

--- push.sh
+++ push.sh
@@ -7,3 +7,6 @@ TARGET_NAMESPACE=${GITHUB_BASE_REF:-temp-test}
 
  kubectl get namespace ${TARGET_NAMESPACE} || kubectl create namespace ${TARGET_NAMESPACE}
  kubectl apply -n ${TARGET_NAMESPACE} -f config.yaml
+ # Give some time for pod to start
+ sleep 90
+ 
